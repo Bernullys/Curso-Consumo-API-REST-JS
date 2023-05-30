42751ffb79980caed361fca44c105ce678d640bd
badr <bernardoantoniod@gmail.com>
2023-05-30 14:13:01 -0400

    clase3 empiezo los retos pero que desastre dejes

--- clase3/main.js
+++ clase3/main.js
@@ -1,18 +1,25 @@
 const API = "https://api.thedogapi.com/v1/images/search";
 
-// cuando cargamos una api, lo primero que tenemos que hacer es convertirla a algo que js pueda comprender.
-// la convertimos a un json
+async function fetchData(urlApi) {
+    const response = await fetch(urlApi);
+    const data = await response.json();
+    return data;
+};
 
-fetch(API)
-    .then(response => response.json())
-    .then(data => { // recibiendo la estructura del json y lo podemos ver ordenada con json viewer y ver que necesitamos.
-        const img = document.querySelector("img");
+const dogFunction = async (urlApi) => {
+    try {
+
+        const img = await fetchData(API);
         img.src = data[0].url;
-        // data[0].url // elemento 0 del array y atributo url  // hay que ponerle la url a la etiqueta imagen en el html.
-    });
+    } catch (Error) {
+        throw new Error("Algo esta mal");
+    };
+};
 
-const refreshButton = docuent.querySelector(".refresh-button");
-refreshButton.addEventListener("click", response);
+dogFunction(API);
+
+// const refreshButton = document.querySelector(".refresh-button");
+// refreshButton.addEventListener("click", aaaa);
 
 
 
